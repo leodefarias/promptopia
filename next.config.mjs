605a11aaaa8c5cf3e1e5d4050515ddc41e16c38a
leodefarias <lf25079@gmail.com>
2024-09-26 14:17:35 -0300

    sign in correction

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverComponentsExternalPackages: ["mongoose"],
-      missingSuspenseWithCSRBailout: false,
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
     }
+    return config
+  }
 }
-  
+
 export default nextConfig;
