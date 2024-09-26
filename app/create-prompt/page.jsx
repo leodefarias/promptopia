c2bbf5ba9986a5f8087c1347dd72aad1ba48cfa7
leodefarias <lf25079@gmail.com>
2024-09-26 13:29:52 -0300

    other's profile correction

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
+import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 import Form from "@components/Form";
 
@@ -10,46 +10,42 @@ const CreatePrompt = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
-  })
-  
-  const CreatePrompt = async (e) => {
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+
+  const createPrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
 
     try {
-      const response = await fetch('/api/prompt/new', {
-        method: 'POST',
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
         body: JSON.stringify({
           prompt: post.prompt,
           userId: session?.user.id,
-          tag: post.tag
-        })
+          tag: post.tag,
+        }),
       });
 
-      if(response.ok) {
-        router.push('/');
+      if (response.ok) {
+        router.push("/");
       }
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
-  }
-
+  };
 
   return (
-    <Form 
+    <Form
       type='Create'
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmit={CreatePrompt}
+      handleSubmit={createPrompt}
     />
-  )
-}
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,49 +1,37 @@
 "use client";
-import { useEffect, useState,Suspense } from "react";
-import { useSession } from "next-auth/react";
+
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-
-  const { data: session } = useSession();
-
-  const [submitting, setSubmitting] = useState(false);
-
-  const searchParams =  useSearchParams();
-
+  const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     const getPromptDetails = async () => {
-        const response = await fetch(`/api/prompt/${promptId}`)
-        const data =  await response.json();
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-        setPost(
-            {
-                prompt : data.prompt,
-                tag: data.tag,
-            }
-        )
-    }
-    if(promptId){
-        getPromptDetails();
-    }
-  },[promptId]);
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
-    setSubmitting(true);
+    setIsSubmitting(true);
 
-    if(!promptId){
-        return alert("Prompt ID  not found!");
-    }
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -60,26 +48,19 @@ const UpdatePrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
   };
-  
 
   return (
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      ></Form>
+    <Form
+      type='Edit'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
-const EditPrompt = () => {
-    return <Suspense>
-        <UpdatePrompt />
-    </Suspense>
-}
-
-export default EditPrompt;
+export default UpdatePrompt;
